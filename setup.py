634c5814580a59354c5e8d2ae2627ba32b83d524
Miloslav Pojman <miloslav.pojman@gmail.com>
2014-02-28 17:46:40 +0100

    Add setup.py script.

---
+++ setup.py
@@ -0,0 +1,36 @@
+
+import io
+from setuptools import setup
+
+
+VERSION = (0, 1)
+VERSION_STR = ".".join(map(str, VERSION))
+
+
+url='https://github.com/mila/spadl'
+
+try:
+    long_description = io.open('README.rst', "tr", encoding="utf-8").read()
+except IOError:
+    long_description = "See %s" % url
+
+
+setup(
+    name='spadl',
+    version=VERSION_STR,
+    description='This package provides a standard logging handler which writes log records to DbgLog.',
+    long_description=long_description,
+    author='Miloslav Pojman',
+    author_email='miloslav.pojman@gmail.com',
+    url=url,
+    py_modules=['spadl'],
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: System :: Logging'
+    ],
+    zip_safe=False,
+)
